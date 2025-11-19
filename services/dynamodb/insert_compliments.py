1aa41a1ef9eb52a20ee542517c05f670d988211d
Lucynda <lucynda.carlier@gmail.com>
2025-11-19 11:17:53 +0100

    feat(bd) : script insert fonctionnel

--- services/dynamodb/insert_compliments.py
+++ services/dynamodb/insert_compliments.py
@@ -1,7 +1,12 @@
+import os
 import csv
 import boto3
 
-# Connexion à LocalStack
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CSV_PATH = os.path.join(BASE_DIR, "compliments.csv")
+
+print("Using CSV:", CSV_PATH)
+
 dynamodb = boto3.resource(
     "dynamodb",
     endpoint_url="http://localhost:4566",
@@ -12,8 +17,23 @@ dynamodb = boto3.resource(
 
 table = dynamodb.Table("compliments")
 
-with open("compliments.csv", encoding="utf-8") as f:
-    reader = csv.DictReader(f)
+# Récupérer tous les items
+response = table.scan()
+items = response.get("Items", [])
+
+print(f"{len(items)} items found. Deleting...")
+
+# Supprimer chaque item
+for item in items:
+    table.delete_item(
+        Key={"id": item["id"]}
+    )
+    print("Deleted:", item["id"])
+
+print("Table content wiped successfully.")
+
+with open(CSV_PATH, encoding="utf-8-sig") as f:
+    reader = csv.DictReader(f, delimiter=';')
 
     for row in reader:
         item = {
@@ -22,9 +42,7 @@ with open("compliments.csv", encoding="utf-8") as f:
             "message": row["message"],
             "image": row["image"]
         }
-
         table.put_item(Item=item)
-
-        print(f"Inserted: {item}")
+        print("Inserted:", item)
 
 print("All compliments have been inserted.")
