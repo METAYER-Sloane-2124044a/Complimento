be30cdd2324813f8483b872dbd4239d07ff7a5d4
Lucynda <lucynda.carlier@gmail.com>
2025-11-19 09:02:00 +0100

    feat(db) : ajout dans main.tf

--- main.tf
+++ main.tf
@@ -5,15 +5,32 @@ provider "aws" {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
-    endpoints {
-        s3 = "http://localhost:4566"
-        dynamodb    = "http://localhost:4566"
-        lambda      = "http://localhost:4566"
-        apigateway  = "http://localhost:4566"
-    }
-    s3_use_path_style = true
+
+  endpoints {
+    s3         = "http://localhost:4566"
+    dynamodb   = "http://localhost:4566"
+    lambda     = "http://localhost:4566"
+    apigateway = "http://localhost:4566"
+  }
+
+  s3_use_path_style = true
 }
 
+
+# S3 bucket
 resource "aws_s3_bucket" "complimento_bucket" {
-    bucket = "complimento-bucket"
+  bucket = "complimento-bucket"
+}
+
+# DynamoDB
+resource "aws_dynamodb_table" "compliments" {
+  name         = "compliments"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "id"
+
+  attribute {
+    name = "id"
+    type = "N"
+  }
 }
