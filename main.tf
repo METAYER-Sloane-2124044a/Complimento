8475225123398f82f0983727ed7cab38db55e03a
Lucynda <lucynda.carlier@gmail.com>
2025-11-20 09:19:34 +0100

    fix(front) : big big fix

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-    access_key                  = "test"
-    secret_key                  = "test"
-    region                      = "us-east-1"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-    endpoints {
-        s3 = "http://localhost:4566"
-        dynamodb    = "http://localhost:4566"
-        lambda      = "http://localhost:4566"
-        apigateway  = "http://localhost:4566"
-        iam     = "http://localhost:4566"
-    }
-    s3_use_path_style = true
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  endpoints {
+    s3         = "http://localhost:4566"
+    dynamodb   = "http://localhost:4566"
+    lambda     = "http://localhost:4566"
+    apigateway = "http://localhost:4566"
+    iam        = "http://localhost:4566"
+  }
+  s3_use_path_style = true
 }
 
 
@@ -42,6 +42,104 @@ resource "null_resource" "insert_compliments" {
   provisioner "local-exec" {
     command = "py services/dynamodb/insert_compliments.py"
   }
+}
+
+
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.complimento_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.complimento_bucket.id
+  key          = "index.html"
+  content      = file("${path.module}/web/index.html")
+  content_type = "text/html"
+}
+
+
+resource "aws_s3_object" "compliments_html" {
+  bucket       = aws_s3_bucket.complimento_bucket.id
+  key          = "compliments.html"
+  content      = file("${path.module}/web/compliments.html")
+  content_type = "text/html"
+  etag         = filemd5("${path.module}/web/compliments.html")
+}
+
+resource "aws_s3_object" "style_css" {
+  bucket       = aws_s3_bucket.complimento_bucket.id
+  key          = "style.css"
+  content      = file("${path.module}/web/assets/style.css")
+  content_type = "text/css"
+  etag         = filemd5("${path.module}/web/assets/style.css")
+}
+
+
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.complimento_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource = [
+          "${aws_s3_bucket.complimento_bucket.arn}/*",
+        ],
+      },
+    ],
+  })
+  depends_on = [aws_s3_bucket_public_access_block.public_access_block]
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.complimento_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = true
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_object" "image_cute" {
+  bucket       = aws_s3_bucket.complimento_bucket.id
+  key          = "cute.jpg"
+  source       = "${path.module}/services/dynamodb/images/cute.jpg"
+  content_type = "image/jpeg"
+  etag         = filemd5("${path.module}/services/dynamodb/images/cute.jpg")
+}
+
+resource "aws_s3_object" "image_motivation" {
+  bucket       = aws_s3_bucket.complimento_bucket.id
+  key          = "motivation.jpg"
+  source       = "${path.module}/services/dynamodb/images/motivation.jpg"
+  content_type = "image/jpeg"
+  etag         = filemd5("${path.module}/services/dynamodb/images/motivation.jpg")
+}
+
+resource "aws_s3_object" "image_romantique" {
+  bucket       = aws_s3_bucket.complimento_bucket.id
+  key          = "romantique.jpg"
+  source       = "${path.module}/services/dynamodb/images/romantique.jpg"
+  content_type = "image/jpeg"
+  etag         = filemd5("${path.module}/services/dynamodb/images/romantique.jpg")
+}
+
+resource "aws_s3_object" "image_drole" {
+  bucket       = aws_s3_bucket.complimento_bucket.id
+  key          = "drole.jpg"
+  source       = "${path.module}/services/dynamodb/images/drole.jpg"
+  content_type = "image/jpeg"
+  etag         = filemd5("${path.module}/services/dynamodb/images/drole.jpg")
 }
 
 # Init Lambda
@@ -134,4 +232,8 @@ resource "aws_api_gateway_stage" "dev" {
   stage_name    = "dev"
   rest_api_id   = aws_api_gateway_rest_api.lambda.id
   deployment_id = aws_api_gateway_deployment.deployment.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
