102c5cec73b5a36019fef06ef4e6ecb182ede861
Sloane Métayer <sloane.metayer@sfr.fr>
2025-11-26 11:23:55 +0100

    feat : ajout de l'affichage de l'url localstack

--- output.tf
+++ output.tf
@@ -7,3 +7,8 @@ output "page_accueil" {
   value       = "${var.ENDPOINT_URL}/${aws_s3_bucket.complimento_bucket.bucket}/${var.INDEX_PAGE}"
   description = "URL de la page d'accueil"
 }
+
+output "localstack_website_url" {
+  description = "URL du site S3 hébergé sur LocalStack"
+  value       = "http://${aws_s3_bucket.complimento_bucket.bucket}.s3-website.localhost.localstack.cloud:4566"
+}
